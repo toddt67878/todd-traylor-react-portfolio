f42b73d7235637938e960b9152f2871054b5a544
Todd Traylor <toddt67878@yahoo.com>
2019-07-03 19:41:38 -0500

    Modify app.js and addportfolio

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Todd Traylor React Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+      <div className="app">
+        <h1>Todd Traylor Portfolio</h1>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
   }
