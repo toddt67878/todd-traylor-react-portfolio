ed1f8e0c08c019fbfdf11f8ad9e3de8a399f9827
Todd Traylor <toddt67878@yahoo.com>
2019-07-03 22:48:26 -0500

    Modified portfolio-container.js with current and past employer names

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,14 +3,26 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-  // State
-  // Lifecycle hooks
+  constructor() {
+    super();
+
+    console.log("Portfolio container has rendered");
+  }
+
+  portfolioItems() {
+    const data = ["Palmer Traylor Inc", "Shopko Pharmacy 696", "Hamilton County Drug Store"];
+
+    return data.map(item => {
+      return <PortfolioItem />;
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Portfolio items go here updated...</h2>
 
-        <PortfolioItem />
+        {this.portfolioItems()}
       </div>
     );
   }
