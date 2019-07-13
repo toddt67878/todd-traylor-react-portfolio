4385604437374b22d05d4df55c0e4bdd92247087
Todd Traylor <toddt67878@yahoo.com>
2019-07-12 23:15:06 -0500

    Updates through Rendering the Blog Details to the Screen

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor() {
@@ -33,15 +34,11 @@ class Blog extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h2>Blog</h2>
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
 
-        <div>
-          <Link to="/about-me">Read more about myself</Link>
-        </div>
-      </div>
-    );
+    return <div>{blogRecords}</div>;
   }
 }
 
