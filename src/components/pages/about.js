e50ba328aefd4dcd09d1f6ec23bb12d4fbd08266
Todd Traylor <toddt67878@yahoo.com>
2019-07-15 16:02:08 -0500

    Update through Heroku Deployment for React JS Applications

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,20 @@
 import React from "react";
+import profilePicture from '../../../static/assets/images/bio/headshot.jpg';
 
 export default function() {
-  return <div>About</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+      Todd has spent his career as a pharmacist in retail, hospital, and consultant practices. He has a strong desire to make things that will improve his industry through accessibility.
+      </div>
+    </div>
+  );
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,5 +1,45 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/gomarley-1969.jpg";
 
 export default function() {
-  return <div>Contact</div>;
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">620-451-0135</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">https://www.linkedin.com/in/todd-traylor-0b51b32b/</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Norton, KS</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
